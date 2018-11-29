a28d5749d6edeac6f7079cb82da2dfc2f88ef922
Dnny <mcmillandanny@gmail.com>
2018-11-29 17:40:26 -0500

    added styles

--- src/containers/AnimateText.js
+++ src/containers/AnimateText.js
@@ -3,8 +3,11 @@ import { Wave } from 'react-animated-text';
 
   export const Wave1 = () => (
     <div >
-      <Wave className="loaderText" text="Old Spice" effectDuration={4} effect="verticalFadeOut" effectChange={.3} effectDirection='down'
+      <Wave className="loaderText" text="Old Spice" effectDuration={5} effect="verticalFadeOut" effectChange={0.1} effectDirection='down'
         iterations={1}/>
+        <audio controls>
+            <source src="/assets/audio/jingle.mp3" type="audio/mpeg"/>
+        </audio>
     </div>
   )
   
